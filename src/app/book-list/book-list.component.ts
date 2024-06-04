a4a6e5a5a799ac0c2d109ecb02ff5d03618ddb27
cristiangithub2 <gabriel.g.gg578@gmail.com>
2024-06-03 23:03:22 -0300

    lista de libros

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
 export class BookListComponent {
-
+  book = {
+    "name": "El nombre del viento",
+    "category": "Fantasía",
+    "price": "45000",
+    "stock": "4",
+    "image": "assets/img/viento.jpeg"
+  }
 }
